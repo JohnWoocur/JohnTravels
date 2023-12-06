b0eb5e322c0040415fb427771edbedc16a9d5c0e
ad-dilax <antonydilaxsan@gmail.com>
2023-12-06 23:59:17 +0530

    add package module upadted ad

---
+++ admin_dashboard/A-add_package.php
@@ -0,0 +1,29 @@
+<?php
+include_once ("db-connection.php");
+
+$Pack_title = $_POST['Pack_title'];
+$Pack_img = $_POST['Pack_img'];
+$Description = $_POST['Description'];
+$Group_size = $_POST['Group_size'];
+$Trip_date = $_POST['Trip_date'];
+$Days = $_POST['Days'];
+$Night = $_POST['Night'];
+//$Sale_price = $_POST['Sale_price'];
+$Reqular_price = $_POST['Reqular_price'];
+$Discount = $_POST['Discount'];
+$Category = $_POST['Category'];
+$Map = $_POST['Map'];
+$Api_key = $_POST['Api_key'];
+
+$check = mysqli_query($conn,"INSERT INTO package(Pack_title,Pack_img, Description, Group_size, Trip_date, Days, Night, Reqular_price, Discount, Category, Map, Api_key) VALUES('$Pack_title','$Pack_img','$Description','$Group_size','$Trip_date','$Days','$Night','$Reqular_price','$Discount','$Category','$Map','$Api_key')");
+if($check)
+{	
+	echo "Package Added successfully!";
+	header("Location: db-add-package.php");
+}
+else 
+{
+  echo "Package Added failure!";
+  header("Location: db-add-package.php");
+}
+?>
