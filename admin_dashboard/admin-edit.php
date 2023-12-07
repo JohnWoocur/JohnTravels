e9f332a711c29e5575cb55caf21ddb654e1c21c6
SivaharanBOOM <Sivabook404@outlook.com>
2023-12-07 17:03:57 +0530

    View sub admin full details

--- admin_dashboard/admin-edit.php
+++ admin_dashboard/admin-edit.php
@@ -188,6 +188,30 @@
                     </ul>
                 </div>
             </div>
+            
+            <?php
+            // admin detail show
+            include 'db-connection.php';
+            if (isset($_GET['Admin_Id'])) {
+                $adminId = $_GET['Admin_Id']; 
+                $query = "SELECT * FROM admins WHERE Admin_Id = $adminId";
+                $result = $conn->query($query);
+
+                if ($result->num_rows > 0) {
+                    $adminData = $result->fetch_assoc();
+                } else {
+                   
+                    echo "Admin not found";
+                    exit;
+                }
+            } else {
+               
+                echo "Admin_Id not provided";
+                exit;
+            }
+            $conn->close();
+            ?>
+
             <div class="db-info-wrap">
                 <div class="row">
                     <div class="col-lg-12">
@@ -199,37 +223,37 @@
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>First name</label>
-                                            <input name="firstname" class="form-control" type="text">
+                                            <input name="firstname" class="form-control" type="text" value="<?php echo $adminData['First_name']; ?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>Last name</label>
-                                            <input name="lastname" class="form-control" type="text">
+                                            <input name="lastname" class="form-control" type="text" value="<?php echo $adminData['Last_name']; ?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>Mobile</label>
-                                            <input name="phone" class="form-control" type="text">
+                                            <input name="phone" class="form-control" type="text" value="<?php echo $adminData['Mobile_number']; ?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>Date</label>
-                                            <input name="phone" class="form-control" type="date">
+                                            <input name="phone" class="form-control" type="date" value="<?php echo $adminData['Date']; ?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>City</label>
-                                            <input name="city" class="form-control" type="text">
+                                            <input name="city" class="form-control" type="text" value="<?php echo $adminData['City']; ?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>Country</label>
-                                            <input name="country" class="form-control" type="text">
+                                            <input name="country" class="form-control" type="text" value="<?php echo $adminData['Country']; ?>">
                                         </div>
                                     </div>
                                     <div class="col-sm-6">
@@ -242,7 +266,7 @@
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                             <label>Email</label>
-                                            <input name="email" class="form-control" type="email">
+                                            <input name="email" class="form-control" type="email" value="<?php echo $adminData['Email']; ?>">
                                         </div>  
                                     </div>
                                     <div class="col-12">
