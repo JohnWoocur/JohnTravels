12d9c951391e38e443e0d0cb67c4d8d824b48c71
SivaharanBOOM <Sivabook404@outlook.com>
2023-12-29 21:36:29 +0530

    last

--- user_dashboard/U-package-booking.php
+++ user_dashboard/U-package-booking.php
@@ -5,31 +5,20 @@ include 'db-connection.php';
 $customer_id=$_SESSION['id'];
 $pack_id=$_POST['pack_id'];
 
-$sql="SELECT customer_more_detail WHERE Customer_Id = $customer_id";
- 
-$result=mysqli_query($conn,$sql);
+$sql="INSERT INTO `package_booking`(`Customer_Id`, `Pack_Id`) VALUES ('$customer_id','$pack_id')";
 
-if($row=mysqli_fetch_assoc($result)){
 
-    $sql="INSERT INTO `package_booking`(`Customer_Id`, `Pack_Id`) VALUE ('$customer_id','$pack_id')";
-    $result =mysqli_query($conn,$sql);
-    if ($result){
-        $_SESSION['Booksmsg']="Booking added successfuly!";
-        header("Location: user-dashboard.php");
-        exit();
-        
-    }else{
-        $_SESSION['Bookemsg']="Booking addeing Failed!";
-        header("Location: user-dashboard.php");
-        exit();
-}
-  }
-
-  else{
-    header("Location: user-edit.php");
-    $_SESSION['Emsg']="Please add your full details!";
+$result =mysqli_query($conn,$sql);
+if ($result){
+    $_SESSION['Booksmsg']="Booking added successfuly!";
+	header("Location: user-dashboard.php");
+	exit();
     
-  }
+}else{
+    $_SESSION['Bookemsg']="Booking addeing Failed!";
+	header("Location: user-dashboard.php");
+	exit();
+}
 
 
 ?>
